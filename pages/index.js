30e77451bee16b0294b97e624c947d326f9e10db
LordCracken <vladislav070201@gmail.com>
2022-12-22 04:31:00 +0300

    prettify index page code

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,23 @@ const DUMMY_MEETUPS = [
   {
     id: 'm1',
     title: 'A First Meetup',
-    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/great-ocean-road-174028267-1494616481.jpg',
+    image:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/great-ocean-road-174028267-1494616481.jpg',
     address: 'Some address 5, 12345, Some City',
-    description: 'This is a first meetup!'
+    description: 'This is a first meetup!',
   },
   {
     id: 'm2',
     title: 'A Second Meetup',
-    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/great-ocean-road-174028267-1494616481.jpg',
+    image:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/great-ocean-road-174028267-1494616481.jpg',
     address: 'Some address 10, 12345, Some City',
-    description: 'This is a second meetup!'
-  }
-]
+    description: 'This is a second meetup!',
+  },
+];
 
 const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
-}
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
 
-export default HomePage
+export default HomePage;

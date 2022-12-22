6b3db95b9e6c16531a4626a14b27a810643415c2
LordCracken <vladislav070201@gmail.com>
2022-12-22 23:47:02 +0300

    add meetup detail page

---
+++ pages/[meetupId].js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/great-ocean-road-174028267-1494616481.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
